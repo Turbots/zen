0b596be8be58c8c952ac2a6c415d4e96b00ea014
Dieter Hubau <dhubau@pivotal.io>
2020-11-02 21:26:07 +0100

    Initial Commit

---
+++ src/main/java/com/tanzu/zen/ZenApplication.java
@@ -0,0 +1,13 @@
+package com.tanzu.zen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanzu/zen/ZenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanzu.zen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
