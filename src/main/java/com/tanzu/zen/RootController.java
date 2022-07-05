632bebf0ec8c4fd4fcba78478002a7dcd4bee1e6
Dieter Hubau <dhubau@pivotal.io>
2022-07-05 09:54:54 +0200

    Updated for hello fluxys

--- src/main/java/com/tanzu/zen/RootController.java
+++ src/main/java/com/tanzu/zen/RootController.java
@@ -34,14 +34,18 @@ public class RootController implements CommandLineRunner {
     @Override
     public void run(String... args) {
         List<String> sayings = List.of(
-                "The temptation to give up is strongest just before victory.",
-                "The goal in life is to die young, but to do as late as possible.",
-                "Don't speak if it doesn't improve on silence.",
-                "A thousand-mile journey begins with just one step.",
-                "A strong man overcomes an obstacle, a wise man goes the whole way.",
-                "Don’t be afraid to go slowly. Be afraid of stopping.",
-                "A hut full of laughter is richer than a palace full of sadness.",
-                "A good teacher opens the door for you, but you must enter the room by yourself."
+                "If you think it’s expensive to hire a professional, wait until you hire an amateur.",
+                "Programming is not a zero-sum game. Teaching something to a fellow programmer does not take it away from you.",
+                "The key in such a transition to continuous delivery is to expect things to get worse before you’ll be able to make them better.",
+                "To successfully implement continuous delivery, you need to change the culture of how an entire organization views software development efforts.",
+                "If you adopt only one agile practice, let it be retrospectives. Everything else will follow.",
+                "The key to following the continuous delivery path is to continually question your own assumptions about what’s possible.",
+                "It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change.",
+                "Showing a strong success and visible benefits is key to getting others to agree to try your way of doing things.",
+                "Any code commit that passes the entire CI/CD pipeline should be considered a candidate for production.",
+                "Bad KPIs always lead to bad behaviour.",
+                "Practice the philosophy of continuous improvement. Get a little bit better every single day.",
+                "People should understand shared goals. The sort of closed, need-to-know thinking gets in the way of product and technical delivery. Everyone should know how systems work if they are interested."
         );
 
         sayings.forEach(this.messageService::save);
