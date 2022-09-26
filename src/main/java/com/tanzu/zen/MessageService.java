0240396f62ab08addb06c39c3c81828f9e64fa7f
Dieter Hubau <dhubau@gmail.com>
2022-09-26 21:21:05 +0200

    SBM: applied recipe 'boot-2.7-3.0-dependency-version-update'

--- src/main/java/com/tanzu/zen/MessageService.java
+++ src/main/java/com/tanzu/zen/MessageService.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
